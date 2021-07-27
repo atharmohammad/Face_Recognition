5bcddfe17d47bec760195eb6369a6c33cac9c858
atharmohammad <mohd.rule123@gmail.com>
2021-07-27 12:05:12 +0530

    face_recognised with a rectangle on the face coordinates in the frame

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0) 
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while True:
     ret,frame = cap.read()
@@ -9,7 +10,15 @@ while True:
     if ret == False:
         continue
 
-    cv2.imshow("Face_Recognition Activate", frame)
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray_frame,1.3,5) # returns list of tuples having co-ordinates of faces in the frame
+
+    for (x,y,w,h) in faces : 
+        cv2.rectangle(gray_frame, (x,y),(x+w,y+h),(255,0,0),2 )
+
+    cv2.imshow("Face_Recognition Activate", gray_frame)
+
     key_pressed = cv2.waitKey(1) & 0xFF
     if key_pressed == ord('f'):
         break
