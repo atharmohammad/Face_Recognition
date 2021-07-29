e4c5283c829ac910b9e495cbfeb74c429a3282ee
atharmohammad <mohd.rule123@gmail.com>
2021-07-29 12:00:29 +0530

    detecting eye

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -4,6 +4,7 @@ import os
 
 cap = cv2.VideoCapture(0) # initialising the camera object
 face_cascade =  cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 data_path = "./data/"  # folder path to where face data files will be saved
 face_data = [] # face data array
@@ -61,12 +62,12 @@ while True:
 
     gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # converting frame to gray scale to save memory
     faces = face_cascade.detectMultiScale(gray_frame,1.3,5)  # detecting face using haarcascade
-
+    
     for (x,y,w,h) in faces :
         offset = 10
         face_section = gray_frame[y-offset:y+h+offset,x-offset:x+w+offset] # extracting the face section 
         face_section = cv2.resize(face_section,(100,100))
- 
+
         output = knn(train_dataset,face_section.flatten()) # now using KNN to predict the face name
         pred_name = names[int(output[0])]
         pred_precentage = str(output[1]) + "%"
@@ -74,6 +75,12 @@ while True:
         cv2.putText(gray_frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
         cv2.putText(gray_frame,pred_precentage,(x+w,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
         cv2.rectangle(gray_frame, (x,y),(x+w,y+h),(255,0,0),2 )
+
+        eyes = eye_cascade.detectMultiScale(gray_frame)
+
+        print(eyes)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(gray_frame, (ex,ey),(ex+ew,ey+eh),(255,0,0),2 )
 
     cv2.imshow("Face Recognition",gray_frame)
     key_pressed = cv2.waitKey(1) & 0xFF # by doing AND operation with 0xFF we convert 32 bit into an 8-bit so to compare it to ASCII val
